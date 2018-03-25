047381afcc0bfeaf1ebbbf50e01268189b10a4a8
txema <txema@nabla.net>
2018-03-26 01:35:09 +0200

    stc refactor

--- src/adata/gui/text.py
+++ src/adata/gui/text.py
@@ -41,16 +41,70 @@ MARKERS = {
 
 
 class StyledText(stc.StyledTextCtrl):
+    ''' 
+    Scintilla text control
 
+    See https://docs.wxpython.org/wx.stc.StyledTextCtrl.html
+    '''
     _style_cache = [None]
 
-    _history = []
-    _history_index = None
+
+
+    def echo(self, text=None, style=None, lf=True, marker=None, icon=None):
+        ''' The print method
+        '''
+        if not style in self._style_cache:
+            self._style_cache.append(style)
+            self.StyleSetSpec(len(self._style_cache)-1, style)
+
+        if text is None: text=""
+
+        line = self.MarkerLineFromHandle(self.marker["prompt"])-1
+        pos = self.GetLineEndPosition(line)
+
+        self.InsertText(pos, '%s%s' % (text,'\n' if lf else '') )
+
+        if style is not None:
+            length = len(text)
+            self.StartStyling(pos=pos, mask=0xFF)
+            self.SetStyling(length=length, style=self._style_cache.index(style))
+
+        mark = None        
+        if icon is not None:
+            if icon in self.mark_number.keys():
+                markerNumber = self.mark_number[icon]
+            else:
+                markerNumber = 0
+            mark = self.MarkerAdd(line, markerNumber)
+
+        if marker is not None:
+            self.marker[marker] = self.MarkerAdd(line) if mark is None else mark                
+            #TODO: Modify a previously marked line (EX: Task... [OK-1:21] )
+ 
+        self.EnsureCaretVisible()
+
+        
+
+    def write(self, text):
+        if len(text)>80:
+
+            if text[0] in ("(","[","{"):
+                text = ',\n'.join(text.split(','))
+                #o = json.loads(text)
+                #text = json.dumps(o, indent=4, sort_keys=True)
+
+        wx.CallAfter(self.echo, 
+            text = text, 
+            lf = False, 
+        )
+
+
 
     def __init__(self, *args, **kwargs):
+
+        
         stc.StyledTextCtrl.__init__(self, *args, **kwargs)
 
-        #self.SetScrollWidth(800)
         self.SetScrollWidthTracking(True)
 
         self._styles = PlatformStyle()
@@ -80,95 +134,69 @@ class StyledText(stc.StyledTextCtrl):
             self.mark_number[name] = number
         
 
-    def write(self, text):
-        if len(text)>80:
-
-            if text[0] in ("(","[","{"):
-                text = ',\n'.join(text.split(','))
-
-                #o = json.loads(text)
-                #text = json.dumps(o, indent=4, sort_keys=True)
-
-        wx.CallAfter(self.echo, 
-            text = text, 
-            lf = False, 
-        )
-
-
-
-    # d88888b  .o88b. db   db  .d88b.  
-    # 88'     d8P  Y8 88   88 .8P  Y8. 
-    # 88ooooo 8P      88ooo88 88    88 
-    # 88~~~~~ 8b      88~~~88 88    88 
-    # 88.     Y8b  d8 88   88 `8b  d8' 
-    # Y88888P  `Y88P' YP   YP  `Y88P'
-
-
-    def echo(self, text=None, style=None, lf=True, marker=None, icon=None):
-        
-        if not style in self._style_cache:
-            self._style_cache.append(style)
-            self.StyleSetSpec(len(self._style_cache)-1, style)
-
-        if text is None: text=""
-
-        line = self.MarkerLineFromHandle(self.marker["prompt"])-1
-        pos = self.GetLineEndPosition(line)
-
-        self.InsertText(pos, '%s%s' % (text,'\n' if lf else '') )
-
-        if style is not None:
-            length = len(text)
-            self.StartStyling(pos=pos, mask=0xFF)
-            self.SetStyling(length=length, style=self._style_cache.index(style))
 
 
 
 
-       
-        mark = None        
-        if icon is not None:
-            if icon in self.mark_number.keys():
-                markerNumber = self.mark_number[icon]
-            else:
-                markerNumber = 0
-            mark = self.MarkerAdd(line, markerNumber)
-
-        if marker is not None:
-            self.marker[marker] = self.MarkerAdd(line) if mark is None else mark            
-            
-            #Si ya existe modificar linea
-
-
-
-        
-        #if br: 
-        #    line = self.MarkerLineFromHandle(self.marker["prompt"])
-        #    pos = self.GetLineIndentPosition(line)
-        #    self.InsertText(pos, '\r\n' )
-        
-        self.EnsureCaretVisible()
-
-        
-
-
-
-
-
-
+#  .o88b.  .d88b.  d8b   db .d8888.  .d88b.  db      d88888b 
+# d8P  Y8 .8P  Y8. 888o  88 88'  YP .8P  Y8. 88      88'     
+# 8P      88    88 88V8o 88 `8bo.   88    88 88      88ooooo 
+# 8b      88    88 88 V8o88   `Y8b. 88    88 88      88~~~~~ 
+# Y8b  d8 `8b  d8' 88  V888 db   8D `8b  d8' 88booo. 88.     
+#  `Y88P'  `Y88P'  VP   V8P `8888Y'  `Y88P'  Y88888P Y88888P 
 
 
 class Console(StyledText):
-    ''' See https://docs.wxpython.org/wx.stc.StyledTextCtrl.html
+    ''' Styled text window with input prompt.
     '''
 
+    __history = []
+    __history_index = None
     __zoom = 0
     __find = ""
 
-    _special_keycodes = [
-        wx.WXK_LEFT, wx.WXK_UP, wx.WXK_RIGHT, wx.WXK_DOWN,
-        wx.WXK_RETURN, wx.WXK_TAB, wx.WXK_BACK, wx.WXK_DELETE
+    __special_keys = [
+        wx.WXK_LEFT, 
+        wx.WXK_UP, 
+        wx.WXK_RIGHT, 
+        wx.WXK_DOWN,
+        wx.WXK_RETURN, 
+        wx.WXK_TAB, 
+        wx.WXK_BACK, 
+        wx.WXK_DELETE
     ]
+
+
+    def __init__(self, *args, **kwargs):
+        StyledText.__init__(self, *args, **kwargs)
+
+        self.SetMarginType(1, stc.STC_MARGIN_SYMBOL)
+        self.SetMarginWidth(1, 20)
+        self.SetCaretStyle(2)
+        self.SetCaretForeground(wx.Colour(0, 255, 0))
+
+        self.AddText("\n")  # A blank line before prompt, echo always writes here
+        
+        try:
+            self.prompt = sys.ps1 # Create a prompt 
+        except AttributeError:
+            self.prompt = ">>> "
+
+        self.AddText(self.prompt)
+        self.marker["prompt"] = self.MarkerAdd(1, self.mark_number["prompt"])
+        self.Bind(wx.EVT_KEY_DOWN, self.OnKeyDown)
+
+
+
+
+    def GotoPrompt(self, text=None):
+        line = self.MarkerLineFromHandle(self.marker["prompt"])
+        pos = self.GetLineIndentPosition(line)+len(self.prompt)
+        self.GotoPos( pos )
+        if text is not None:
+            self.DeleteRange(pos, self.GetLength()-pos)
+            if text!="": 
+                self.AddText(text)
 
 
     def OnWrap(self, e):
@@ -225,40 +253,6 @@ class Console(StyledText):
     
 
 
-    def __init__(self, *args, **kwargs):
-        StyledText.__init__(self, *args, **kwargs)
-
-        self.SetMarginType(1, stc.STC_MARGIN_SYMBOL)
-        self.SetMarginWidth(1, 20)
-        self.SetCaretStyle(2)
-        self.SetCaretForeground(wx.Colour(0, 255, 0))
-
-
-        # A blank line before prompt, echo writes here
-        self.AddText("\n")
-        
-        # Create a propmt line
-        try:
-            self.prompt = sys.ps1
-        except AttributeError:
-            self.prompt = ">>> "
-
-        self.AddText(self.prompt)
-        self.marker["prompt"] = self.MarkerAdd(1, self.mark_number["prompt"])
-        
-        self.Bind(wx.EVT_KEY_DOWN, self.OnKeyDown)
-
-
-
-    def GotoPrompt(self, delete=False, cmd=""):
-        line = self.MarkerLineFromHandle(self.marker["prompt"])
-        pos = self.GetLineIndentPosition(line)+len(self.prompt)
-        self.GotoPos( pos )
-        if delete or cmd!="":
-            self.DeleteRange(pos, self.GetLength()-pos)
-            if cmd!="": self.AddText(cmd)
-
-
 
 
 
@@ -283,54 +277,49 @@ class Console(StyledText):
         line_current = self.GetCurrentLine()
         line_prompt = self.MarkerLineFromHandle(self.marker["prompt"])
 
-        if line_current!=line_prompt: return event.Skip()
+        if line_current!=line_prompt: 
+            return event.Skip()
 
         pos_prompt = self.GetLineIndentPosition(line_prompt)
         pos = self.GetCurrentPos()
 
-        # Don't touch my prompt
-        if pos-pos_prompt<len(self.prompt):
+        if pos-pos_prompt<len(self.prompt): # U can't touch
             self.GotoPrompt()
             return 
 
-        # Check key
         keycode = event.GetKeyCode()
-        if not keycode in self._special_keycodes: return event.Skip()
+        if not keycode in self.__special_keys:  # Not interested
+            return event.Skip()
         
         if keycode in [wx.WXK_LEFT, wx.WXK_BACK]:
-            if(pos-pos_prompt)==len(self.prompt): return        
+            if (pos-pos_prompt)==len(self.prompt): # Don't go
+                return  
 
-        
-        if keycode==wx.WXK_RETURN:
-            
-            cmd = self.GetLine(line_prompt)
-            if cmd[0:len(self.prompt)]!=self.prompt:  return event.Skip()
-            cmd = cmd[len(self.prompt):].strip()
+        if keycode==wx.WXK_RETURN:    
+            # Command
+            cmd = self.GetLine(line_prompt)[len(self.prompt):].strip()
             if cmd=="": return
+            
             self.echo()
             self.echo(self.prompt+cmd,"fore:#ffff00,bold")
-            self.GotoPrompt(delete=True)
-            self._history.insert(0, cmd)
-            self._history_index = 0
+            self.GotoPrompt('')
+            self.__history.insert(0, cmd)
+            self.__history_index = 0
             self.Enter(cmd)
             return
-            
-        index = self._history_index
-        if index is None: return
+
+        # Command history
+        if self.__history_index is None: return
         
         if keycode==wx.WXK_UP:
-            
-            self.GotoPrompt(cmd=self._history[self._history_index])
-            if (self._history_index+1)<len(self._history): self._history_index+=1
+            self.GotoPrompt( self.__history[self.__history_index] )
+            if (self.__history_index+1)<len(self.__history): self.__history_index+=1
             return
 
         if keycode==wx.WXK_DOWN:
-            if index is None: return
-            self.GotoPrompt(cmd=self._history[self._history_index])
-            if self._history_index>0: self._history_index-=1
+            self.GotoPrompt( self.__history[self.__history_index] )
+            if self.__history_index>0: self.__history_index-=1
             return
 
-
-            
         event.Skip()
             
